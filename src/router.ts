e159b99fa4134d55544117e83c85766b9a8775da
gabrielbustosdev <gabrielbustosdev@gmail.com>
2025-02-19 12:54:29 -0300

    configuraciones iniciales

---
+++ src/router.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+const router = Router();
+
+// Routings
+
+router.get("/", (req, res) => {
+    res.send("Desde GET");
+});
+
+router.post("/", (req, res) => {
+    res.send("Desde POST");
+});
+
+router.put("/", (req, res) => {
+    res.send("Desde PUT");
+});
+
+router.patch("/", (req, res) => {
+    res.send("Desde PATCH");
+});
+
+router.delete("/", (req, res) => {
+    res.send("Desde DELETE");
+});
+
+export default router;
