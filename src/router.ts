6a57985b3b9b481cba4d921a7da65ab179be35ba
gabrielbustosdev <gabrielbustosdev@gmail.com>
2025-02-21 00:50:59 -0300

    documentacion swagger

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,121 @@
-import { Router } from 'express'
+import { Router } from 'express'
 import { body, param } from 'express-validator'
 import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updateProduct } from './handlers/product'
 import { handleInputErrors } from './middleware/index'
 
 const router = Router()
 
-// Routing
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       properties:
+ *          id:
+ *            type: integer
+ *            description: The auto-generated ID of the product
+ *            example: 1
+ *          name:
+ *            type: string
+ *            description: The name of the product
+ *            example: "Product 1"   
+ *          price:
+ *            type: number
+ *            description: The price of the product
+ *            example: 100
+ *          availability:
+ *            type: boolean
+ *            description: The availability of the product
+ *            example: true
+ */
+
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     description: Return a list of products
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
 router.get('/', getProducts)
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     tags: 
+ *       - Products
+ *     description: Return a product based on its unique ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: The ID of the product to retrieve 
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: Product not found
+ *       400:
+ *         description: Bad Request - Invalid ID provided
+ */
 router.get('/:id',
-    // Validación
     param('id').isInt().withMessage('ID no válido'),
     handleInputErrors,
-    // Handler
     getProductById
 )
 
-router.post('/', 
-    // Validación
+/**
+ * @swagger
+ * /api/products:
+ *   post:
+ *     summary: Create a new product
+ *     tags: 
+ *       - Products
+ *     description: Returns a new record in the database
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Product 1"
+ *                 description: The name of the product
+ *               price:
+ *                 type: number
+ *                 example: 100
+ *                 description: The price of the product
+ *     responses:
+ *       201:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       400:
+ *         description: Bad Request - Invalid request body
+ */
+router.post('/',
     body('name')
         .notEmpty().withMessage('El nombre de Producto no puede ir vacio'),
     body('price')
@@ -25,12 +123,56 @@ router.post('/',
         .notEmpty().withMessage('El precio de Producto no puede ir vacio')
         .custom(value => value > 0).withMessage('Precio no válido'),
     handleInputErrors,
-    // Handler
     createProduct
 )
 
-router.put('/:id', 
-    // Validación
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   put:
+ *     summary: Update a product by ID
+ *     tags: 
+ *       - Products
+ *     description: Update a product based on its unique ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: The ID of the product to update
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Product 1"
+ *                 description: The name of the product
+ *               price:
+ *                 type: number
+ *                 example: 100
+ *                 description: The price of the product
+ *               availability:
+ *                 type: boolean
+ *                 example: true
+ *                 description: The availability of the product
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       400:
+ *         description: Bad Request - Invalid ID or invalid input data
+ *       404:
+ *         description: Product not found
+ */
+router.put('/:id',
     param('id').isInt().withMessage('ID no válido'),
     body('name')
         .notEmpty().withMessage('El nombre de Producto no puede ir vacio'),
@@ -41,23 +183,73 @@ router.put('/:id',
     body('availability')
         .isBoolean().withMessage('Valor para disponibilidad no válido'),
     handleInputErrors,
-    // Handler
     updateProduct
 )
 
-router.patch('/:id', 
-    // Validación
+/**
+ * @swagger 
+ * /api/products/{id}:
+ *   patch:
+ *     summary: Update the availability of a product by ID
+ *     tags: 
+ *       - Products
+ *     description: Update the availability of a product based on its unique ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: The ID of the product to update
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       400:
+ *         description: Bad Request - Invalid ID 
+ *       404:
+ *         description: Product not found
+ */
+router.patch('/:id',
     param('id').isInt().withMessage('ID no válido'),
     handleInputErrors,
-    // Handler
     updateAvailability
 )
 
-router.delete('/:id', 
-    // Validación
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   delete:
+ *     summary: Delete a product by ID
+ *     tags: 
+ *       - Products
+ *     description: Delete a product based on its unique ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: The ID of the product to delete
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               value: "Producto Eliminado"
+ *       400:
+ *         description: Bad Request - Invalid ID
+ *       404:   
+ *         description: Product not found
+ */
+router.delete('/:id',
     param('id').isInt().withMessage('ID no válido'),
     handleInputErrors,
-    // Handler
     deleteProduct
 )
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express' 
 import colors from 'colors'
+import swaggerUI from 'swagger-ui-express'
+import swaggerSpec from './config/swagger'
 import router  from './router'
 import db from './config/db'
 
@@ -24,8 +26,7 @@ server.use(express.json())
 
 server.use('/api/products', router)
 
-server.get('/api', (req: Request, res: Response) => {
-    res.json({msg: 'Desde API'})
-})
+// Docs
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
 export default server
